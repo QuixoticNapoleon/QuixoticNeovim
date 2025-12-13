ca8b1bd0a783244d60851445ba47aaa0f67cef19
bh <qn+git@excalibur.computer>
2025-12-13 23:44:24 +0800

    Added more plugins

--- init.lua
+++ init.lua
@@ -106,6 +106,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
+-- UndoTree
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
+
 --------------------------------------------------------
 -- 🪶 GUI and Colour Configuration
 --------------------------------------------------------
